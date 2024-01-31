248d1ff4f72c25c2830baea141facf1210f28bc3
Lukas <lukasdrinkwater97@gmail.com>
2024-01-31 17:17:05 +0000

    initial commit, late and last before removing from local

--- project-inventory-management/app.js
+++ project-inventory-management/app.js
@@ -39,6 +39,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+console.log(mongoose.connection.readyState);
+
 // middleware stack to use the correct routers
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
